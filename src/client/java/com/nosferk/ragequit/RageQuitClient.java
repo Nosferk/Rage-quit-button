c218a210623518a6bc490c5ee4946edf74bd842f
Virk <125840564+Nosferk@users.noreply.github.com>
2025-08-10 21:12:37 -0300

    first commit

---
+++ src/client/java/com/nosferk/ragequit/RageQuitClient.java
@@ -0,0 +1,10 @@
+package com.nosferk.ragequit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RageQuitClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
